9ffb29a6ffbf92dc5d5e8713b39220d38db63aa7
Richard Wu <richardwu1997@gmail.com>
2022-10-19 10:31:54 -0400

    Formatting.

--- 0-setup-apollo/test.js
+++ 0-setup-apollo/test.js
@@ -1,53 +1,67 @@
-const {  HttpLink, ApolloClient, InMemoryCache, ApolloProvider, gql } = require('@apollo/client');
+const {
+  HttpLink,
+  ApolloClient,
+  InMemoryCache,
+  gql,
+} = require("@apollo/client");
 const { ApolloLink, concat } = require("apollo-link");
-const fetch = require('cross-fetch');
+const fetch = require("cross-fetch");
 
-const API_KEY = process.env.TENSOR_API_KEY ?? '';
-if(!API_KEY) throw new Error("please specify envvar TENSOR_API_KEY");
+const API_KEY = process.env.TENSOR_API_KEY ?? "";
+if (!API_KEY) throw new Error("please specify envvar TENSOR_API_KEY");
 
 // Setup Apollo client.
 const authLink = new ApolloLink((operation, forward) => {
   operation.setContext({
     headers: {
-      'X-TENSOR-API-KEY': API_KEY,
+      "X-TENSOR-API-KEY": API_KEY,
     },
   });
   return forward(operation);
 });
-const httpLink = new HttpLink({ uri: 'https://api.tensor.so/graphql', fetch });
+const httpLink = new HttpLink({ uri: "https://api.tensor.so/graphql", fetch });
 const client = new ApolloClient({
   link: concat(authLink, httpLink),
   cache: new InMemoryCache(),
   defaultOptions: {
     query: {
-      fetchPolicy: 'no-cache',
-    }
+      fetchPolicy: "no-cache",
+    },
   },
 });
 
 // Run query.
 (async () => {
-	const resp = await client
-	  .query({
-	    query: gql`
-        query TswapBuyNftTx($buyer: String!, $maxPriceLamports: Decimal!, $mint: String!, $pool: String!) {
-          tswapBuyNftTx(buyer: $buyer, maxPriceLamports: $maxPriceLamports, mint: $mint, pool: $pool) {
-            txs {
-              lastValidBlockHeight
-              tx
-            }
+  const resp = await client.query({
+    query: gql`
+      query TswapBuyNftTx(
+        $buyer: String!
+        $maxPriceLamports: Decimal!
+        $mint: String!
+        $pool: String!
+      ) {
+        tswapBuyNftTx(
+          buyer: $buyer
+          maxPriceLamports: $maxPriceLamports
+          mint: $mint
+          pool: $pool
+        ) {
+          txs {
+            lastValidBlockHeight
+            tx
           }
         }
-	    `,
-			variables: {
-			  "buyer": "ACZtzuJpk9LXAN6x9rYPpBDZnLXSf5svK6vN56vhjmcM",
-			  "maxPriceLamports": "2900000000",
-			  "mint": "DqbDQvm9Va1RmnsnUPHxpU87oBLHMMKvFqC3DhZUdHDN",
-			  "pool": "5yEpsbcaN4LmCF1qJ3Dn1L2GmS3fCHMLhfsQvacCy76U"
-			},
-	  });
+      }
+    `,
+    variables: {
+      buyer: "ACZtzuJpk9LXAN6x9rYPpBDZnLXSf5svK6vN56vhjmcM",
+      maxPriceLamports: "2900000000",
+      mint: "DqbDQvm9Va1RmnsnUPHxpU87oBLHMMKvFqC3DhZUdHDN",
+      pool: "5yEpsbcaN4LmCF1qJ3Dn1L2GmS3fCHMLhfsQvacCy76U",
+    },
+  });
 
-	const results = resp.data.tswapBuyNftTx;
+  const results = resp.data.tswapBuyNftTx;
   console.log(results);
   // {
   //   txs: [
